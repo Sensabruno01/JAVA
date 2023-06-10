652c692052bfa972b6a3078b674abb33210dc47b
sensabruno01@gmail.com <sensabruno01@gmail.com>
2023-06-10 12:38:55 +0530

    inverted Half-pyramid

--- src/Patterns.java
+++ src/Patterns.java
@@ -28,14 +28,26 @@ public class Patterns {
 //**
 //***
 //****
-        int n =4;
-        for(int i=1;i<=n;i++){
-            for(int j = 1; j<=i; j++){
+//        int n =4;
+//        for(int i=1;i<=n;i++){
+//            for(int j = 1; j<=i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+//Print the pattern-inverted half pyramid
+//        ****
+//        ***
+//        **
+//        *
+        int n = 4;
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+
                 System.out.print("*");
             }
             System.out.println();
         }
-
-
     }
 }
