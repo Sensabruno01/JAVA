87ff3324888a37fdabc08e447008d9617e1ef628
sensabruno01@gmail.com <sensabruno01@gmail.com>
2023-06-10 12:13:55 +0530

    Half triangle

--- src/Patterns.java
+++ src/Patterns.java
@@ -6,19 +6,36 @@ public class Patterns {
 //   *   *
 //   *   *
 //   *****
-        int n = 4;
-        int m = 5;
-        //outer loop
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
+//        int n = 4;
+//        int m = 5;
+//        //outer loop
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= m; j++) {
+//
+//                if (i == 1 || j == 1 || i == n || j == m) {
+//                    System.out.print("*");
+//                } else {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
 
-                if (i == 1 || j == 1 || i == n || j == m) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
+
+//Print the pattern -half pyramid
+
+//*
+//**
+//***
+//****
+        int n =4;
+        for(int i=1;i<=n;i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
             }
             System.out.println();
         }
+
+
     }
 }
