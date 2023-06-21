376e49fef57873b8a66ff08bec32d5057b0f3082
sensabruno01@gmail.com <sensabruno01@gmail.com>
2023-06-21 22:05:32 +0530

    Addition of two numbers

--- src/Functions.java
+++ src/Functions.java
@@ -2,18 +2,39 @@ import java.util.*;
 import java.util.Scanner;
 public class Functions {
 
-    public static void printMyName( String name){
+    //To print name
+//    public static void printMyName(String name)
+//    {
+//
+//        System.out.println(name);
+//        return;
+//    }
+//
+//    public static void main(String[] args)
+//    {
+//
+//           Scanner input=new Scanner(System.in);
+//           String name=input.next();
+//
+//           printMyName(name);
+//    }
 
-        System.out.println(name);
-        return;
+    //Make a function to add two numbers
+    public static int addTwoNumbers(int a,int b)
+    {
+         int sum;
+         sum=a+b;
+         return sum;
     }
 
-    public static void main(String[] args){
 
-           Scanner input=new Scanner(System.in);
-           String name=input.next();
+     public static void main(String[] args)
+     {
+         Scanner input=new  Scanner(System.in);
+         int a = input.nextInt();
+         int b = input.nextInt();
 
-           printMyName(name);
-    }
-
+         int sum = addTwoNumbers(a,b);
+         System.out.println(sum);
+     }
 }
