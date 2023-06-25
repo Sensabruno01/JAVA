a78c794a452f5717d070a953fef3151c80cf6c49
sensabruno01@gmail.com <sensabruno01@gmail.com>
2023-06-25 14:22:14 +0530

    loops basic

---
+++ src/Loops_Practise.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Loops_Practise {
+
+    public static void main(String args[]){
+//Print numbers from 1 to n
+//        Scanner input=new Scanner(System.in);
+//        System.out.println("Enter the number:");
+//        int n = input.nextInt();
+//
+//        for(int i=1;i<=n;i++)
+//        {
+//            System.out.println(i);
+//        }
+//
+//        System.out.println("Loop exit");
+
+
+//Code to write sum of first ten natural numbers
+//      int sum=0;
+//      for(int i=1;i<=10;i++)
+//      {
+//          sum=sum+i;
+//
+//      }
+//        System.out.println(sum);
+
+// Write a program that prompts the user to input a positive integer.
+// It should then print the multiplication table of that number.
+
+
+//        Scanner input=new Scanner(System.in);
+//        int num=input.nextInt();
+//
+//        if(num>0){
+//
+//            int n =1;
+//            for(int i=1;i<=10;i++)
+//            {
+//                n=num*i;
+//                System.out.println(num + " " + "*" + " "+ i + " " + " " + "="+" "+ n);
+//            }
+//
+//        }
+//        else if(num==0){
+//            System.out.println("Anything multiplied by 0 is 0");
+//        }
+//        else{
+//            System.out.println("Enter a positive integer");
+//        }
+//
+  }
+
+
+}
