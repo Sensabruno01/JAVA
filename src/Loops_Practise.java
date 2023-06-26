f8c8d38e78b213714c54fcc241f9c3d91805e337
sensabruno01@gmail.com <sensabruno01@gmail.com>
2023-06-26 10:55:23 +0530

    Reverse number

--- src/Loops_Practise.java
+++ src/Loops_Practise.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Loops_Practise {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 //Print numbers from 1 to n
 //        Scanner input=new Scanner(System.in);
 //        System.out.println("Enter the number:");
@@ -50,7 +50,66 @@ public class Loops_Practise {
 //            System.out.println("Enter a positive integer");
 //        }
 //
-  }
+// Write a program to find the factorial value of any number entered through the keyboard
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("enter the number:");
+//        int num = input.nextInt();
+//
+//        int fact = 1;
+//        for (int i = 1; i <= num; i++) {
+//            fact = fact * i;
+//        }
+//        System.out.println("The factorial of" + " "+ num + " is "+fact);
+
+//        Two numbers are entered through the keyboard.
+//        Write a program to find the value of one number raised to the power of another.
+//        (Do not use Java built-in method)
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter the number:");
+//        int num= input.nextInt();
+//        System.out.println("Enter the power:");
+//        int pow= input.nextInt();
+//        int result=1;
+//        for(int i=1;i<=pow;i++)
+//        {
+//            result=result*num;
+//        }
+//        System.out.println("The result is "+result);
+
+        //Write a program that prompts the user to input an integer and then outputs the number with the digits reversed.
+        // For example, if the input is 12345, the output should be 54321
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the number to be reversed:");
+        int number= input.nextInt();
+        int reverse=0;
+        //using for loop
+
+//        for(int i=number;i>0;i/=10)
+//        {
+//           int remainder = i%10;
+//           reverse = reverse * 10 +remainder;
+//
+//        }
+//        System.out.println("The reverse of the "+number+" is "+reverse);
+
+        //using while loop
+
+//        while (number>0)
+//        {
+//            int remainder=number%10;
+//            reverse=reverse*10+remainder;
+//            number=number/10;
+//        }
+//        System.out.println(reverse);
 
 
+
+
+
+
+   }
 }
+
+
