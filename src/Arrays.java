e3de1c321beb0a2f5281322683d4faad46f125f1
sensabruno01@gmail.com <sensabruno01@gmail.com>
2023-07-03 18:39:56 +0530

    2D array intro

--- src/Arrays.java
+++ src/Arrays.java
@@ -23,26 +23,56 @@ public class Arrays {
 
         //Taking array size from user
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the size of the array");
-        int size= input.nextInt();
-        int[] marks = new int[size];
-        //for-loop for input
-        for(int i =0;i<size;i++){
-            marks[i]=input.nextInt();
-        }
-        System.out.println("Enter a number");
-        int num= input.nextInt();
-        //for-loop for output
-        for(int i =0;i<size;i++)
-        {
-            if(marks[i]==num)
-            {
-                System.out.println("The "+ num  + "is index's place is "+ i);
-            }
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter the size of the array");
+//        int size= input.nextInt();
+//        int[] marks = new int[size];
+//        //for-loop for input
+//        for(int i =0;i<size;i++){
+//            marks[i]=input.nextInt();
+//        }
+//        System.out.println("Enter a number");
+//        int num= input.nextInt();
+//        //for-loop for output
+//        for(int i =0;i<size;i++)
+//        {
+//            if(marks[i]==num)
+//            {
+//                System.out.println("The "+ num  + "is index's place is "+ i);
+//            }
+//
+//        }
+//Becsaue we did not give the values for this array,each index will have value 0
 
+        //2-D arrays
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the size of rows:");
+        int rows = input.nextInt();
+        System.out.println("Enter the size of columns:");
+        int column = input.nextInt();
+
+        int[][] numbers= new int[rows][column];
+
+        //Nested loop for taking input
+
+        for(int i=0;i<rows;i++)
+        { //Outer loop
+            for(int j=0;j<column;j++)
+            { //inner loop for columns
+                numbers[i][j] = input.nextInt();
+            }
         }
-//Beacsue we did no give the values for this array,each index will have value 0
+
+        //Nested loop for printing the output
+        for(int i =0;i<rows;i++)
+        {
+            for(int j = 0;j<column;j++)
+            {
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
 
     }
 
