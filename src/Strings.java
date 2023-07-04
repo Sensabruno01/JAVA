923abdde6594524c7a427b3bbc0204d5f4c55261
sensabruno01@gmail.com <sensabruno01@gmail.com>
2023-07-04 19:30:28 +0530

    Some string methods

--- src/Strings.java
+++ src/Strings.java
@@ -11,9 +11,43 @@ public class Strings {
 
         Scanner input = new Scanner(System.in);
 //        String name=input.next();
-        String name = input.nextLine(); //to take more than one word as string input
-        System.out.println("Your name is "+ name);
+//        String name = input.nextLine(); //to take more than one word as string input
+//        System.out.println("Your name is "+ name);
 
+        //Concatenation
+
+        String fname="Tony";
+        String lname="Stark";
+        String fullname=fname + " "+ lname ;
+        System.out.println(fullname);
+//        System.out.println(fullname.length());
+//Going through each character of the string
+//        for(int i = 0;i<fullname.length();i++){
+//
+//            System.out.println(fullname.charAt(i));
+//        }
+
+        //To compare two strings-compare function()
+//          if(fname.compareTo(lname)==0){
+//              System.out.println("fname and lname are equal");
+//          }
+//          else if(fname.compareTo(lname)>0){
+//              System.out.println("fname is bigger than lname");
+//          }
+//          else {
+//              System.out.println("lname is bigger");
+//          }
+
+        //To compare
+
+//        if(new String ("Tony")==new String  ("Tony"))
+//        {
+//            System.out.println("Equal");
+//        }                                  //Here by using ==,its showing wrong-reason will learn in string builder
+//        else
+//        {
+//            System.out.println("Not equal");
+//        }
 
     }
 }
