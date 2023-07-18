3edf1d6c7a8e7e2da24efaafed78a96ea7b0c278
sensabruno01@gmail.com <sensabruno01@gmail.com>
2023-07-18 08:21:10 +0530

    Set bit

--- src/BitManipulation.java
+++ src/BitManipulation.java
@@ -1,17 +1,25 @@
 public class BitManipulation {
 
     public static void main(String[] args) {
+        //Get Bit
+//        int n = 5;//0101
+//        int pos = 2;
+//        int bitMask = 1<<pos;
+//
+//        if((bitMask & n)==0){
+//            System.out.println("Bit is 0");
+//        }
+//        else{
+//            System.out.println("Bit is 1");
+//        }
+
+        //Set Bit
 
         int n = 5;//0101
-        int pos = 2;
+        int pos = 1;
         int bitMask = 1<<pos;
 
-        if((bitMask & n)==0){
-            System.out.println("Bit is 0");
-        }
-        else{
-            System.out.println("Bit is 1");
-        }
-
+        int check= bitMask | n;
+        System.out.println(check);
     }
 }
