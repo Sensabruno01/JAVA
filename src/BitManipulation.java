ee782ffc318dcc2b3b0fc92afbece85fdf75e57e
sensabruno01@gmail.com <sensabruno01@gmail.com>
2023-07-18 07:26:00 +0530

    Get bit

---
+++ src/BitManipulation.java
@@ -0,0 +1,17 @@
+public class BitManipulation {
+
+    public static void main(String[] args) {
+
+        int n = 5;//0101
+        int pos = 2;
+        int bitMask = 1<<pos;
+
+        if((bitMask & n)==0){
+            System.out.println("Bit is 0");
+        }
+        else{
+            System.out.println("Bit is 1");
+        }
+
+    }
+}
