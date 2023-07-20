81860450b729bad0fa09e76e1925c5b567e3a4cd
sensabruno01@gmail.com <sensabruno01@gmail.com>
2023-07-20 17:58:58 +0530

    Recursion Basic-print numbers from 1 to 5

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,18 +1,36 @@
 public class Recursion {
 
-    public static void printNumb(int n){
-        if(n==0)
-        {
-            return;
-        }
-        System.out.println(n);
-        printNumb(n-1);
+//    public static void printNumb(int n) {
 
-    }
-    public static void main(String args[])
-    {
+    //Print numbers from 5 to 1
+//        if(n==0)----->BASE CASE
+//        {
+//            return;
+//        }
+//        System.out.println(n);
+//        printNumb(n-1);
+//
+//    }
+//     public static void main(String args[])
+//    {
+//
+//        int n = 5;
+//        printNumb(n);//5
+//    }
 
-        int n = 5;
-        printNumb(n);//5
-    }
+    //Print numbers from 1 to 5
+//    public static void printNumb(int n){
+//          if(n == 6){
+//              return;
+//          }
+//        System.out.println(n);
+//          printNumb(n+1);
+//    }
+//        public static void main(String args[]) {
+//            int n = 1;
+//            printNumb(n);
+//        }
+//        }
 }
+
+
