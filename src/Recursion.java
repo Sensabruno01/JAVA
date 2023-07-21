6ba4b963dd632895ec748fa2747d4ad376cea060
sensabruno01@gmail.com <sensabruno01@gmail.com>
2023-07-21 10:30:34 +0530

    Recursion Basic-print fibonacci

--- src/Recursion.java
+++ src/Recursion.java
@@ -63,6 +63,24 @@ public class Recursion {
 //      int ans =calcFactorial(n);
 //        System.out.println(ans);
 //    }
+
+    //Print fibonacci series
+//    public static void printFibonacci(int a,int b,int n){
+//        if(n==0){
+//            return;
+//        }
+//        int c = a + b;
+//        System.out.println(c);
+//        printFibonacci(b,c,n-1);
+//    }
+//    public static void main(String args[]){
+//        int a =0;
+//        int b =1;
+//        System.out.println(a);
+//        System.out.println(b);
+//        int n =7;
+//        printFibonacci(a,b,n-2);
+//    }
 }
 
 
