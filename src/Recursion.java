27acd7a1348167fcedb2a00e60557443ca1f2312
sensabruno01@gmail.com <sensabruno01@gmail.com>
2023-07-21 11:05:29 +0530

    Recursion Basic-Stack height concept,n=1 and n =logn

--- src/Recursion.java
+++ src/Recursion.java
@@ -81,6 +81,47 @@ public class Recursion {
 //        int n =7;
 //        printFibonacci(a,b,n-2);
 //    }
+      //To calculate power(Stack height =n)
+//     public static int calcPower(int x,int n){
+//         if(n==0){
+//             return 1;//base case 1
+//         }
+//         if (x==0){
+//             return 0;//base case 2
+//         }
+//         int xPownm1 = calcPower(x,n-1);//kaam
+//         int xPown = x * xPownm1;
+//         return xPown;
+//     }
+//    public static void main(String args[]){
+//
+//         int x =2;
+//         int n = 5;
+//         int ans = calcPower(x,n);
+//        System.out.println(ans);
+//    }
+    //Calculate power(stack height = logn)
+    public static int calcPower(int x,int n){
+        if(n==0){//base case 1
+            return 1;
+        }
+        if(x==0){
+            return 0;//base case 2
+        }
+        //if n is even
+        if(n%2==0){
+          return  calcPower(x,x/2) * calcPower(x,n/2);
+        }
+        else{//n is odd
+           return calcPower(x,n/2)*calcPower(x, n/2) * x;
+        }
+    }
+    public static void main(String args[]){
+         int x =2;
+         int n = 5;
+         int ans = calcPower(x,n);
+         System.out.println(ans);
+    }
 }
 
 
